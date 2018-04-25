17cb171473ba566027abdf4da02e26bac6fedfac
Mauricio Araya V <maraya@gmail.com>
2018-04-25 08:44:57 -0600

    Update pack-n-upload.sh

--- lambda-packager/pack-n-upload.sh
+++ lambda-packager/pack-n-upload.sh
@@ -2,5 +2,15 @@
 
 set -e
 
+cd "${BITBUCKET_CLONE_DIR}/app"
+if [ -e requirements.txt ]; then
+   if [ "${PY_LOCAL_PATH}x" == "x" ]; then
+     PY_LOCAL_PATH="lib"
+   fi
+   pip install -r requirements.txt -t "${PY_LOCAL_PATH}"
+   sudo find . -name __pycache__ -delete
+fi
+chmod -R 0777 $(pwd)
+zip -r package.zip $(ls *.py) "${PY_LOCAL_PATH}"
 pwd
-ls -Alh
+ls -Alh package.zip
